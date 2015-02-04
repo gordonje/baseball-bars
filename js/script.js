24b6ef3b11fd0b0442dab0a846d5b9112338e019
James Gordon <gordo@mwc-225054.dhcp.missouri.edu>
2015-02-03 20:06:24 -0600

    First commit of chart

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+var playerData;
+
 $(document).ready(function() {
-    console.log("Hello world.")
+	getData();
 });
 
+function getData() {
+	$.getJSON('js/baseballcard.json', function(data) {
+		playerData = data;
+		drawChart();
+	});
+}
 
+function drawChart() {
+	$.each(playerData.stats, function(i, item) {
 
+		console.log(item)
 
+		var width = item.HR;
 
+		$('.chart').append('<div class="bar" style="width: ' + width * 10 + 'px">' + item.HR + '</div>');
+	});
+}
 
